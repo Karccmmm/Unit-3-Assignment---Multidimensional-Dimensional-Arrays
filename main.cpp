08e1ad4929ec62d889e5a8b25cc52654cac7fbb0
Kareem Mahmoud <92833648+Karccmmm@users.noreply.github.com>
2021-11-09 02:12:01 +0000

    new line

---
+++ main.cpp
@@ -0,0 +1,88 @@
+//Student Name: Kareem Mahmoud
+
+//Teacher: Dr.Tyson McMillan
+
+//A program to practice working with multi-dimensional arrays
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+
+{
+
+      const int CITY = 2;
+
+      const int WEEK = 7;
+
+ 
+
+    int temperature[CITY][WEEK];
+
+
+    cout << "Enter all temperature for a week of first city and then second city. \n";
+
+
+
+
+/*Program Expansion Start */
+//array for city 1 and city 2 
+//as required they have been named 
+int NewYorkCity [WEEK];
+int GothamCity [WEEK];
+//First run the loop to store the data for the two cities
+//new york city
+cout<<"=======Expanded program input values======="<<endl;
+for(int j = 0; j < WEEK; ++j)
+    {
+        cout << "New York City Day "<< j + 1 << " Temperature: ";
+            cin >> NewYorkCity[j];
+    }
+//g city
+for(int j = 0; j < WEEK; ++j)
+    {
+        cout << "Miami Day "<< j + 1 << " Temperature: ";
+            cin >> GothamCity[j];
+    }
+
+//Update the temperature array with these two cities
+for (int i=0;i<CITY;i++)
+{
+    for (int j=0;j<WEEK;j++)
+    {
+        if (i==0)
+        {
+        temperature[i][j] = NewYorkCity[j];
+        }
+        else if (i==1)
+        {
+           temperature[i][j] = GothamCity[j] ;
+        }
+    }
+}
+
+//print the temperature array
+cout<<"=======Expanded program output======="<<endl;
+for (int i=0;i<CITY;i++)
+{
+    for (int j=0;j<WEEK;j++)
+    {
+        if (i==0)
+        {
+        cout<<"New York City Day "<< j+1<< " = "<< temperature[i][j]<<endl; 
+        }
+        else if (i==1)
+        {
+         cout<<"Miami Day "<< j+1<< " = "<< temperature[i][j]<<endl; 
+        }
+    }
+}
+
+
+/*Program Expansion Ends */
+
+
+    return 0;
+}
